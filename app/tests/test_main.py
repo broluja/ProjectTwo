7add09f71380aa3b5c490b739bc1d50356bef335
Branko <olujic.branko@gmail.com>
2023-03-05 00:45:33 +0100

    adjusting test module for postgresql

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,15 +3,13 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.config import settings
+from app.config import settings as sttg
 from app.db import Base
 from app.main import app
 
-MYSQL_URL_TEST = \
-    f"{settings.DB_HOST}://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOSTNAME}:" \
-    f"{settings.DB_PORT}/{settings.DB_NAME_TEST}"
+POSTGRE_URL_TEST = f"postgresql://{sttg.DB_USER}:{sttg.DB_PASSWORD}@{sttg.DB_HOST}:{sttg.DB_PORT}/{sttg.DB_NAME_TEST}"
 
-engine = create_engine(MYSQL_URL_TEST, echo=True)
+engine = create_engine(POSTGRE_URL_TEST, echo=True)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
